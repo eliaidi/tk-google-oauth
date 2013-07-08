82625d83c1af25953c3c8af8de10505ede379ecc
Tahuri Killer <tahuri.killer@gmail.com>
2013-07-08 16:19:38 -0300

    Se crea una entidad que representa al usuario logeado con el sso de google, falta terminar de armar la clase

---
+++ src/main/java/ar/com/tahurikiller/entidades/UsuarioGoogle.java
@@ -0,0 +1,12 @@
+package ar.com.tahurikiller.entidades;
+
+/**
+ * @author Tahuri Killer
+ *
+ */
+public class UsuarioGoogle {
+	private String nombre;
+	private String email;
+	private String imagen;
+	private String linkPerfil;
+}
