06473136ecfbc8fcf3abb4b5ac1ae4a777accbb5
Tahuri Killer <tahuri.killer@gmail.com>
2014-04-18 02:33:29 -0300

    I update this class adding one constructor and a property of type GoogleClient

--- src/main/java/ar/com/tahurikiller/GoogleMediator.java
+++ src/main/java/ar/com/tahurikiller/GoogleMediator.java
@@ -1,5 +1,7 @@
 package ar.com.tahurikiller;
 
+import ar.com.tahurikiller.entity.GoogleClient;
+
 /**
  * <h1>Mediator</h1>
  * <p>
@@ -11,4 +13,29 @@ package ar.com.tahurikiller;
  */
 public class GoogleMediator {
 
+	private GoogleClient client;
+
+	/**
+	 * 
+	 * @param client
+	 *            for be used
+	 */
+	public GoogleMediator(GoogleClient client) {
+		setClient(client);
+	}
+
+	/**
+	 * @return the client
+	 */
+	private GoogleClient getClient() {
+		return client;
+	}
+
+	/**
+	 * @param client
+	 *            the client to set
+	 */
+	private void setClient(GoogleClient client) {
+		this.client = client;
+	}
 }
