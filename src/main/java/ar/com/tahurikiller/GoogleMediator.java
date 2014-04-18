3d10303c20584b5fa146312f2ca23bfb226c9ceb
Tahuri Killer <tahuri.killer@gmail.com>
2014-04-18 01:59:25 -0300

    I add class for that be mediator with Google

---
+++ src/main/java/ar/com/tahurikiller/GoogleMediator.java
@@ -0,0 +1,14 @@
+package ar.com.tahurikiller;
+
+/**
+ * <h1>Mediator</h1>
+ * <p>
+ * This Class is the mediator with Google
+ * </p>
+ * 
+ * @author tahuri
+ * 
+ */
+public class GoogleMediator {
+
+}
